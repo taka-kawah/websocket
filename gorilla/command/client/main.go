958732a2a808e874e17a245ddab2096d3ac5bfcc
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-11-09 21:31:56 +0900

    とりあえずサーバー建てた

--- gorilla/command/client/main.go
+++ gorilla/command/client/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
+func main() {
+	client := &http.Client{}
+	resp, err := client.Get("http://server:18443/ws")
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
--- gorilla/command/server/main.go
+++ gorilla/command/server/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+	w.Write([]byte("this is ws"))
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("this is websocket server. please get /ws"))
+	})
+	http.HandleFunc("/ws", serveWs)
+	fmt.Println("start listening at 18443...")
+	err := http.ListenAndServe(":18443", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
