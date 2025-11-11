713a053fcbb90720b2b736bb614beb8175e0e6b4
河原 <106893036+taka-kawah@users.noreply.github.com>
2025-11-12 01:50:01 +0900

    wsエンドポイント作成

--- FastAPI/command/client/main.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-resp = requests.get("http://server:8000")
-print(resp.content.decode('utf-8'))
--- FastAPI/command/server/main.py
+++ FastAPI/command/server/main.py
@@ -1,7 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import asyncio
+from random import randint
 
 app = FastAPI()
 
 @app.get("/")
 def serve_home():
     return "this is websocket server"
+
+@app.websocket("/ws")
+async def serve_ws(ws: WebSocket):
+    await ws.accept()
+    
+    async def send():
+        try:
+            for i in range(1, randint(5, 10)):
+                await asyncio.sleep(2)
+                await ws.send_text(f"message from server {i}")
+        except (WebSocketDisconnect, RuntimeError, asyncio.CancelledError):
+            print("send切断！")
+        finally:
+            await ws.close()
+    
+    async def recv():
+        try:
+            while True:
+                data = await ws.receive_text()
+                print(data)
+        except (WebSocketDisconnect, RuntimeError, asyncio.CancelledError):
+            print("recv切断！")
+    
+    
+    send_task = asyncio.create_task(send())
+    recv_task = asyncio.create_task(recv())
+    _, pending = await asyncio.wait(
+        [send_task, recv_task],
+        return_when=asyncio.FIRST_COMPLETED
+    )
+    for task in pending:
+        task.cancel()
+        try:
+            await task
+        except WebSocketDisconnect:
+            pass
+        except asyncio.CancelledError:
+            pass
